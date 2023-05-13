adc1c0f6cc5f3de944025442876029d77f139d64
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-05-13 18:35:08 +0530

    creating header

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,3 +2,10 @@ import axios from "axios";
 
 const BASE_URL  = "https://api.themoviedb.org/3"
 const TMDB_TOKEN = import.meta.env.VITE_APP_TMBD_TOKEN;
+
+const headers = {
+    AUthorization: "bearer" +
+    TMDB_TOKEN,
+};
+
+export const fetchDATAFromApi = async(url)
