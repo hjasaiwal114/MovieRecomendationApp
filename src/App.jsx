9d6672fe7b22054e30437928e06b591233a1674a
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-05-13 19:40:19 +0530

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 
 import { fetchDATAFromApi } from "./utils/api";
+
 function App() {
   
+  const apiTesting = () => {
+      fetchDATAFromApi('/movie/popular')
+  }
 
   return <div className="App">App</div>
     
