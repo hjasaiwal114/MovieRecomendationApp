4299a1872f3b8e17d909996cc7ebddb5a069221b
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-05-24 20:41:52 +0530

    importing homeslice

--- src/store/homeSlice.js
+++ src/store/homeSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 
 
-export const counterSlice = createSlice({
+export const homeSlice = createSlice({
   name: 'home',
   initialState: {
     url: {},
@@ -19,6 +19,6 @@ export const counterSlice = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { getApiConfiguration, getGenres  } = homeSlice.actions
 
-export default counterSlice.reducer
+export default homeSlice.reducer
