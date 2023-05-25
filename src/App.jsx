67c77638902dffa533f79e15c1a176683b3938d4
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-05-25 09:54:13 +0530

    ipoerting redux tool

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react";
 
 import { fetchDataFromApi } from "./utils/api";
 
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from "./store/homeSlice";
+
+
 function App() {
 
   useEffect(() => {
