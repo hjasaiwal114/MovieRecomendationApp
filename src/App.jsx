c20c0a3dd7fc9ef710e72cdb3bc12cf18d344357
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-05-27 16:55:45 +0530

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,15 @@ function App() {
           });
   };
 
-  return (<BrowserRouter>
+  return (<
+    BrowserRouter>
+      <Route>
+          <Route path= "/" element={<Home />} />
+          <Route path="/:mediaType/:id" element={<Details/>}/>
+          <Route path="/search/:query" element={<SearchResult/>} />
+          <Route path="/explore/:mediaType" element={<Explore/>} />
+          <Route path="*" element={<PageNotFound />} />
+      </Route>
   </BrowserRouter>);
     
 }
